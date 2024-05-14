ed13d3238996d005e3b58f1199917fcb8be8c905
jrw208 <jrwright5@googlemail.com>
2024-05-14 15:20:59 +0100

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.astrocast2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.astrocast2 to javafx.fxml;
+    exports com.example.astrocast2;
+}
