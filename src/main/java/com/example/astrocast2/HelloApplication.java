79cddf933f7855f218ae57070b57cf73fdbc5761
AmirH32 <amirhassanali2610@gmail.com>
2024-05-14 15:31:28 +0100

    Works with dynamic screen sizes

--- src/main/java/com/example/astrocast2/HelloApplication.java
+++ src/main/java/com/example/astrocast2/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.astrocast2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        // Retrieve the screen size
+        Screen screen = Screen.getPrimary();
+        double screenWidth = screen.getBounds().getWidth();
+        double screenHeight = screen.getBounds().getHeight();
+        // Creates the screen with the retrieved proportions
+        Scene scene = new Scene(fxmlLoader.load(), screenWidth*0.8, screenHeight*0.8);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
